e4467b09ea51debd90fd23748ce23e084d1d1e19
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-03-08 09:52:30 +0700

    c

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module Note
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.hosts << nil
+    config.hosts << '.environments.katacoda.com'
   end
 end
